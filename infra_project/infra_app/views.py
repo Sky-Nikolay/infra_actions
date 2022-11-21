7fd61ef27e58697ef68df8034ae1b540b84199e6
Николай Гусев <gusevnikolas@yandex.ru>
2022-11-21 19:37:34 +0300

    TESTs check

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось!', status=200)
+    return HttpResponse('У меня получилось!', status_code=200)
 
 
 def second_page(request):
